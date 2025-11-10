85cc493ba10cdf6ad9a605b612c71de54b75d237
francescalb <francesca.l.bleken@sintef.no>
2025-11-10 15:25:39 +0100

    Commented out all problems in ontology and created a test-script for generating yaml keywords file

---
+++ create_yaml.py
@@ -0,0 +1,14 @@
+from tripper import Triplestore
+from tripper.datadoc import get_keywords
+
+ts =  Triplestore('rdflib')
+
+ts.parse('pink_annotation_schema.ttl')
+
+kw = get_keywords()
+
+kw.add_prefix('pink', 'https://w3id.org/pink#')
+
+kw.load_rdf(ts)
+
+kw.save_yaml('generated_kw.yml')
