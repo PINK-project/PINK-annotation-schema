d8ef550b05a53f871a00be046e809627591c73c2
francescalb <francesca.l.bleken@sintef.no>
2025-11-14 15:42:41 +0100

    Added script for creating keyword-yaml.

--- scripts/create_yaml.py
+++ scripts/create_yaml.py
@@ -3,12 +3,12 @@ from tripper.datadoc import get_keywords
 
 ts =  Triplestore('rdflib')
 
-ts.parse('pink_annotation_schema.ttl')
+ts.parse('../pink_annotation_schema.ttl')
 
 kw = get_keywords()
 
 kw.add_prefix('pink', 'https://w3id.org/pink#')
 
-kw.load_rdf(ts)
+kw.load_rdf(ts, redefine='allow')
 
 kw.save_yaml('generated_kw.yml')
