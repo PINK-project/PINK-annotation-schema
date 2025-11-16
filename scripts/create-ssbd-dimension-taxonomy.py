caf508b540d73d3bd2d5dbd43d39206e42fdc35b
Jesper Friis <jesper.friis@sintef.no>
2025-11-16 14:33:39 +0100

    Replaced the shell script with a python script that also adds an ontology

---
+++ scripts/create-ssbd-dimension-taxonomy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Generates ssbd-dimensions.ttl
+
+from pathlib import Path
+
+from tripper import DCTERMS, OWL, RDF, RDFS, Triplestore
+from tripper.datadoc import TableDoc
+from tripper.utils import en
+
+
+thisdir = Path(__file__).resolve().parent
+rootdir = thisdir.parent
+
+ssbd = "https://w3id.org/pink/ssbd-dimensions"
+ver = "0.0.1"
+
+ts = Triplestore("rdflib")
+
+td = TableDoc.parse_csv(
+    rootdir / "sources" / "ssbd_dimension_taxonomy.csv",
+    type=None,
+    prefixes={"pink": "https://w3id.org/pink#"},
+)
+td.save(ts)
+
+# Add Ontology
+ts.add_triples(
+    [
+        (ssbd, RDF.type, OWL.Ontology),
+        (ssbd, OWL.versionIRI, f"https://w3id.org/pink/{ver}/ssbd-dimensions"),
+        (ssbd, DCTERMS.title, en("Categorisation of SSbD dimensions, categories and sub-categories.")),
+    ]
+)
+
+# Remove subClassOf rdfs:Class relations
+ts.remove(predicate=RDFS.subClassOf, object=RDFS.Class)
+
+ts.serialize(rootdir/"ssbd-dimensions.ttl")
