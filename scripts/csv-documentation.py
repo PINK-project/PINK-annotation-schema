e2aba7b130de5c6e2d92af733a6ddc7356426196
Jesper Friis <jesper.friis@sintef.no>
2025-11-24 23:35:20 +0100

    Document classes and properties as csv tables

---
+++ scripts/csv-documentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Generates csv documentation of all PINK concepts
+
+from pathlib import Path
+
+from tripper import DCTERMS, OWL, RDF, Triplestore
+from tripper.datadoc import TableDoc, acquire
+
+
+
+thisdir = Path(__file__).resolve().parent
+rootdir = thisdir.parent
+
+
+ts = Triplestore("rdflib")
+ts.parse("pink_annotation_schema.ttl")
+
+PINK = ts.namespaces["pink"]
+
+pink_concepts = set(s for s in ts.subjects() if s.startswith(str(PINK)))
+dicts = [acquire(ts, iri) for iri in pink_concepts]
+
+classes = [d for d in dicts if OWL.Class in d["@type"]]
+properties = [d for d in dicts if OWL.Class not in d["@type"]]
+
+td_classes = TableDoc.fromdicts(classes)
+td_classes.write_csv("classes.csv")
+
+td_properties = TableDoc.fromdicts(properties)
+td_properties.write_csv("properties.csv")
