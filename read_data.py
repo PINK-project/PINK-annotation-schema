ae49edab2d88a465e3ad2d11fecb693b485c26f5
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2026-02-24 22:25:30 +0100

    Push for debugging

--- read_data.py
+++ read_data.py
@@ -3,23 +3,13 @@ import re
 from pyshacl import validate
 
 from tripper import Triplestore, Literal
-from tripper.datadoc import store, get_keywords, search, acquire
+from tripper.datadoc import store, get_keywords, search, acquire, told
 from tripper.datadoc.tabledoc import TableDoc
 from pandas import read_csv
 from pyld import jsonld
 import json
 import pandas as pd
 import dateutil
-
-# Make list columns as lists. Use regex to separate strings on ',', ' ', ';' and '|' and remove extra whitespace.
-def split_to_list(value):
-    if pd.isna(value) or str(value).strip() == "":
-        return []  # Return empty list if empty or NaN
-    # Split on comma, semicolon, pipe, or space
-    parts = re.split(r'[,\s;|]+', str(value))
-    # Clean whitespace and remove empty strings
-    cleaned = [p.strip() for p in parts if p.strip() != ""]
-    return cleaned
 
 # in column 'accessRights', add 'rights:' prefix to all values that are not empty and that do not have a prefix already.
 def add_prefix(value, prefix='pink'):
@@ -88,8 +78,10 @@ def export_expanded_csv(df: pd.DataFrame, path: str) -> None:
     """
     parts = []
     for col in df.columns:
+        print('processing column:', col)
         # detect if this column has any list entries
         is_list_col = df[col].apply(lambda v: isinstance(v, list)).any()
+        print('is list column:', is_list_col)
         if is_list_col:
             # convert each row's list to a row in a small DF
             sub = pd.DataFrame(
@@ -99,6 +91,7 @@ def export_expanded_csv(df: pd.DataFrame, path: str) -> None:
             )
             # rename all its columns to the original header
             sub.columns = [col] * sub.shape[1]
+            parts.append(sub)
         else:
             # just take the series as a one-column DF
             parts.append(df[[col]])
@@ -109,6 +102,8 @@ def export_expanded_csv(df: pd.DataFrame, path: str) -> None:
     df = df.fillna('')
     # concatenate side by side
     expanded = pd.concat(parts, axis=1)
+    print('expanded columns:', expanded.columns)
+    print('expanded head:', expanded.head())
     expanded.to_csv(path, index=False)
 
 
@@ -128,15 +123,17 @@ kw = get_keywords(theme=None)
 kw.load_yaml('https://pink-project.github.io/PINK-annotation-schema/context/keywords.yaml', redefine='allow')
 
 # Get name of columns that can have more than one value from termdefs. Check the column SingleValue in termdefs. If False, then the value in column "Poperty"
-list_columns = termdefs[termdefs['SingleValue'] == False]['Property'].tolist()
-
+list_columns = termdefs[termdefs['SingleValue'] == False]['IRI'].tolist()
+# Add @id and @type to the list of columns that can have multiple values, because they can also be lists.
+list_columns.extend(['@id', '@type'])
+# Map properties to IRIs
+property_to_iri = dict(zip(termdefs['Property'], termdefs['IRI']))
 
 # Create the computatations documentation dataframe, and copy/move relevant columns from the software documentation dataframe.
 activity_columns = ['inputDatasetType', 'outputDatasetType',
                     'SsbdDimension', 'SsbdSubDimension', 'SsbdSubSubDimension']
 
 comp = sw[activity_columns+['title']]
-comp.rename(columns={"inputDatasetType": "hasInput", "outputDatasetType": "hasOutput"}, inplace=True)
 sw = sw.drop(columns=activity_columns)
 
 # Correct column names
@@ -156,35 +153,32 @@ sw.drop(columns=[col for col in sw.columns if '(comment)' in col], inplace=True)
 
 # We have to decide how to handle Indicator, it is currently a class.
 sw = sw.drop(columns=["indicator", 
-                      #"releaseDate", # Not handled properly
                       ])
 
 
 # Convert releaseDate to ISO format (YYYY-MM-DD)
 sw['releaseDate'] = sw['releaseDate'].apply(lambda x: dateutil.parser.parse(x).isoformat() if pd.notna(x) else None) 
-print(sw['releaseDate'])
-
 
 # Rename some columns to match the ontology
-sw.rename(columns={"modelType": "implementsModel", "identifier": "@id"}, inplace=True)
+#sw.rename(columns={"modelType": "implementsModel", "identifier": "@id"}, inplace=True)
 
 # Correct the values 
-
 # Change lists to lists
+# Change all haeders according to the property_to_iri mapping
+sw.rename(columns=property_to_iri, inplace=True)
 for col in set(list_columns).intersection(sw.columns):
     sw[col] = sw[col].apply(split_to_list)
- 
 # Add prefixes to values
-sw['accessRights'] = sw['accessRights'].apply(add_prefix, prefix='rights')
-sw['@id'] = sw['@id'].apply(add_prefix, prefix='pink')
+sw['dcterms:accessRights'] = sw['dcterms:accessRights'].apply(add_prefix, prefix='rights')
 
-sw['tierLevel'] = sw['tierLevel'].apply(correct_tier_level)
-sw['tierLevel'] = sw['tierLevel'].apply(add_prefix, prefix='pink')
+sw['pink:tierLevel'] = sw['pink:tierLevel'].apply(correct_tier_level)
+sw['pink:tierLevel'] = sw['pink:tierLevel'].apply(add_prefix, prefix='pink')
 
-
-ts = Triplestore('rdflib')
+# Add @id because tripper does not accept dcterms:identifier as identifier for some reason.
+sw['@id'] = sw['dcterms:identifier'].apply(add_prefix, prefix='pink')
 
 # A bit cumbersome to write file, I am sure there are better ways
+
 export_expanded_csv(sw, 'sw_clean.csv')  
 
 
@@ -196,32 +190,19 @@ swdocumentation = TableDoc.parse_csv(
         )
 
 # Save software documentation to triplestore
-swdocumentation.save(ts)
 
-#swdocumentation2 = TableDoc.parse_csv(
-#        'sw_clean_2.csv',
-#        keywords=kw,
-#        #baseiri='https://w3id.org/pink/',
-#        prefixes={'mw': 'https://modelwave.it/ontology/','rights': 'http://publications.europa.eu/resource/authority/access-right/'}
-#        )
-#swdocumentation2.save(ts)
+ts = Triplestore('rdflib')
+v = swdocumentation.save(ts)
+#v = told(swdocumentation.asdicts(), keywords=kw)
 
-
-# Test dataset form Joh
-#jsonld_doc = json.load(open('validation/tests/dataset.jsonld'))
-#d = store(ts, jsonld_doc, keywords=kw)
-
-#datasets = search(ts)
-#acquire(ts, search(ts)[0])
-
-
+print(comp.head())
 # Save the computational entities as activities
 # Create a unique id (@id) for each activity in the comp dspreadsheet
-#comp['@id'] = comp.apply(lambda row: f"https://w3id.org/pink/activity/{row.name}", axis=1)
+comp['@id'] = comp.apply(lambda row: f"https://w3id.org/pink/activity/{row.name}", axis=1)
 
 
 # Add a column that defines that each activity is a prov:Activity
-#comp['@type'] = 'prov:Activity'
+comp['@type'] = 'prov:Activity'
 
 # Add alle values under SsbdDimension, SsvbSubDimension and SsbdSubSubDimension 
 # into the column '@type' as list.
@@ -236,32 +217,38 @@ def merge_ssbd_dimensions(row):
             parts = str(cell).split(',')
             
             # Clean whitespace and add
-            cleaned = [p.strip() for p in parts if p.strip() != ""]
+            cleaned = ['pink:'+p.strip() for p in parts if p.strip() != ""]
             values.extend(cleaned)
     
     return values
 
-#comp['@type'] = comp.apply(lambda row: ['prov:Activity'] + merge_ssbd_dimensions(row), axis=1)
+comp['@type'] = comp.apply(lambda row: ['prov:Activity'] + merge_ssbd_dimensions(row), axis=1)
+ 
 
 # Remove the Ssbd columns
-#comp.drop(columns=[col for col in comp.columns if col.startswith('Ssbd')], inplace=True)
+comp.drop(columns=[col for col in comp.columns if col.startswith('Ssbd')], inplace=True)
 
-
-
-
+# Change all haeders according to the property_to_iri mapping
+comp.rename(columns=property_to_iri, inplace=True)
+for col in set(list_columns).intersection(comp.columns):
+    print('col that may be listcol:', col)
+    comp[col] = comp[col].apply(split_to_list)
+ 
+print('a')
+print(comp.head())
+print('b')
 # A bit cumbersome to write file, I am sure there are better ways
-#export_expanded_csv(comp, 'comp_clean.csv')
+export_expanded_csv(comp, 'comp_clean.csv')
 
 
-#compdocumentation = TableDoc.parse_csv(
-#        'comp_clean.csv',
-#        keywords=kw,
-#        baseiri='https://w3id.org/pink/',
-#        prefixes={'mw': 'https://modelwave.it/ontology/'}
-#        )
+compdocumentation = TableDoc.parse_csv(
+        'comp_clean.csv',
+        keywords=kw,
+        prefixes={'mw': 'https://modelwave.it/ontology/'}
+        )
 
 # Save software documentation to triplestore
-#compdocumentation.save(ts)
+compdocumentation.save(ts)
 
 
 
