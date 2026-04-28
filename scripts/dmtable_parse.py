ce1e6d15916cdef945013c9b4951c88d18bdc05e
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2026-04-28 12:09:25 +0200

    Update dmtable to add the datasets to ts

--- scripts/dmtable_parse.py
+++ scripts/dmtable_parse.py
@@ -1,6 +1,20 @@
 from dlite.table import DMTable
+from dlite import get_instance
+from tripper import Triplestore
+from dlite.dataset import add_dataset
 
+# create triplestore as helper for 
+# making datamodels into rdf
+ts = Triplestore('rdflib')
+
+# Parse the csv table
 dmtable = DMTable.from_csv("datamodels.csv")
 
+# Create the datamodels
+dmtable.get_datamodels()
 
+# get
 
+add_dataset(ts, get_instance('http://pink-project.eu/omics/transcriptPTGS-BMDoutput'))
+
+dmtable.to_triplestore(ts)
